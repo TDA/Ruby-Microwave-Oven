25d362f3381cfac30e517f79dddae0372e06b9cf
Sai Pc <schand31@asu.edu>
2015-12-30 22:24:03 +0530

    Small oven basic version

---
+++ small_oven.rb
@@ -0,0 +1,32 @@
+class SmallOven
+  attr_accessor :contents, :state
+
+  def turn_on
+    @state = 'on'
+    puts "Turning on"
+  end
+
+  def turn_off
+    @state = 'off'
+    puts "Turning off"
+  end
+
+  def keep(item)
+    @contents = item
+  end
+
+  def bake
+    unless @state == 'on'
+      puts "Oven not ON!"
+    end
+    if @contents == nil?
+      puts "Nothing in the oven"
+    end
+    "#{@contents}"
+  end
+end
+
+oven = SmallOven.new
+oven.turn_on
+oven.keep("Pie")
+puts "Serving #{oven.bake}"
