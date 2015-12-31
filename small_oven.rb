40d04e956d6a5ec854b21c72f2327934a39ea628
Sai Pc <schand31@asu.edu>
2015-12-31 22:02:41 +0530

    Fully functioning exception handling with raise-begin-rescue-ensure blocks

--- small_oven.rb
+++ small_oven.rb
@@ -1,3 +1,13 @@
+# exception classes, can inherit from Exception,
+# but practice is StandardError
+class OvenOffError < StandardError
+
+end
+
+class OvenEmptyError < StandardError
+
+end
+
 class SmallOven
   attr_accessor :contents, :state
 
@@ -17,10 +27,11 @@ class SmallOven
 
   def bake
     unless @state == 'on'
-      raise "Oven not ON!"
+      # raise is the equivalent of throw, classType and message
+      raise OvenOffError, "Oven not ON!"
     end
     if @contents.nil?
-      raise "Nothing in the oven"
+      raise OvenEmptyError, "Nothing in the oven"
     end
     "#{@contents}"
   end
@@ -31,14 +42,28 @@ oven.turn_on
 
 items = ['Pie', 'Pizza', 'Coke', nil]
 
+oven.turn_off
 # begin - rescue is like try - catch
 items.each do |item|
   begin
     oven.keep(item)
     puts "Serving #{oven.bake}"
     # capture the exception thrown
-  rescue => error
+  rescue OvenOffError => error
     puts "Error : #{error.message}"
+    #try to turn it on
+    oven.turn_on
+    # executes the same code again :O have never heard of
+    # this in other langs iv written code in
+    puts "Retrying, after turning on"
+    retry
+  rescue OvenEmptyError => error
+    puts "Error : #{error.message}"
+  # ensure is just like finally, so place after
+  # all rescues, and before the end section
+  ensure
+    puts "Ensuring that oven is turned off after everything"
+    oven.turn_off
   end
 
 end
