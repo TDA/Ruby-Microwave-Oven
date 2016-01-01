45136582932173101da3fab7e2e4a244e93e1d53
Sai Pc <schand31@asu.edu>
2016-01-01 19:34:07 +0530

    Setup and teardown added

--- test_small_oven.rb
+++ test_small_oven.rb
@@ -3,39 +3,47 @@ require 'minitest/autorun'
 
 # load the minitest suite, then write tests
 class TestSmallOven < MiniTest::Test
+  def setup
+    puts "Setup"
+    # this needs to be an instance variable instead of local var
+    # (@oven instead of oven) so it retains scope across methods
+    @oven = SmallOven.new
+  end
+
+  def teardown
+    puts "Teardown"
+    @oven.turn_off
+  end
   def test_turn_on
-    oven = SmallOven.new
     # assert for positive and refute for negative assertions, yay!
-    refute_equal(oven.state, "on", "Oven state is on by default!!")
-    oven.turn_on
+    refute_equal(@oven.state, "on", "Oven state is on by default!!")
+    @oven.turn_on
     # first arg => actual value
     # second arg => expected value
     # third => message to display if not matching
-    assert_equal(oven.state, "on", "Oven state is not ON even after switching on!")
+    assert_equal(@oven.state, "on", "Oven state is not ON even after switching on!")
   end
 
   def test_turn_off
-    oven = SmallOven.new
     # first arg => actual value
     # second arg => expected value
     # third => message to display if not matching
-    assert_equal(oven.state, "off", "Oven state is not off by default!!")
-    oven.turn_on
-    refute_equal(oven.state, "off", "Oven state is off even after switching on!")
+    assert_equal(@oven.state, "off", "Oven state is not off by default!!")
+    @oven.turn_on
+    refute_equal(@oven.state, "off", "Oven state is off even after switching on!")
     # other asserts include assert_includes, assert_raises, assert_instance_of
   end
 
   def test_bake
-    oven = SmallOven.new
     assert_raises(OvenOffError, "No exception raised") do
-      oven.bake
+      @oven.bake
     end
     assert_raises(OvenEmptyError, "No exception raised") do
-      oven.turn_on
-      oven.bake
+      @oven.turn_on
+      @oven.bake
     end
-    oven.turn_on
-    oven.contents = "apple"
-    assert_equal(oven.contents, "apple", "Oven contains wrong item")
+    @oven.turn_on
+    @oven.contents = "apple"
+    assert_equal(@oven.contents, "apple", "Oven contains wrong item")
   end
 end
